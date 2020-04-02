562b98e6f9c50d315173ab0c4cd175989080f7e1
Audrey GAZZA <Audrey@macbook-pro-de-gazza.home>
2020-04-02 12:40:57 +0200

    tuto vue.js part 6

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Titre 1</h1>
+        <h2>Titre 2</h2>
+        <p>Message : {{ msg }}</p>
+        <p>Seulement premier mot : {{ onlyFirstWord }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "Hello World"
+
+        }
+    },
+    computed: {
+        onlyFirstWord(){
+            return this.msg.split('')[0].toUpperCase();
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
